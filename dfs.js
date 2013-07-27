2b709e21a53335a930a8e043dc924436880f64ef
Kitson Kelly <dojo@kitsonkelly.com>
2013-07-27 10:47:32 +0100

    Fix detection of non-callback properties of `fs`.
    Fixes #2

--- dfs.js
+++ dfs.js
@@ -8,7 +8,7 @@ define([
 
 	// Iterate through each property of the module
 	for (var f in fs) {
-		if (typeof fs[f] === 'function' && !/(^_|Sync$)/.test(f)) { // It is something we want to convert
+		if (typeof fs[f] === 'function' && !/(^[_A-Z]|^create|^(un)?watch|Sync$)/.test(f)) { // It is something we want to convert
 			dfs[f] = util.asDeferred(fs[f], fs, ~singleArgFunctions.indexOf(f)); // Create a deferred
 		} else {
 			dfs[f] = fs[f]; // Leave alone
